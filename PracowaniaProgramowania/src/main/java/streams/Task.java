19bf062b93e49980e3df294365795c4d0d361e7a
marcin witkowski <marcin.witkowski@e-qsg.com>
2019-10-17 18:19:49 +0200

    task reorder

--- PracowaniaProgramowania/src/main/java/streams/Task.java
+++ PracowaniaProgramowania/src/main/java/streams/Task.java
@@ -13,6 +13,10 @@ import static java.util.stream.Collectors.toList;
 
 public class Task {
 
+    /**
+     * EASY
+     */
+
     public static List<String> toUpperCaseOldJava(List<String> collection) {
         List<String> newCollection = new ArrayList<>();
         for (String element : collection) {
@@ -22,36 +26,6 @@ public class Task {
     }
 
     public static List<String> toUpperCase(List<String> collection) {
-        // ToDo
-        return null;
-    }
-
-    public static Person getOldestPersonOldJava(List<Person> people) {
-        Person oldestPerson = new Person("", 0);
-        for (Person person : people) {
-            if (person.getAge() > oldestPerson.getAge()) {
-                oldestPerson = person;
-            }
-        }
-        return oldestPerson;
-    }
-
-    public static Person getOldestPerson(List<Person> people) {
-        // ToDo
-        return null;
-    }
-
-    public static Map<Boolean, List<Person>> partitionAdultsOldJava(List<Person> people) {
-        Map<Boolean, List<Person>> map = new HashMap<>();
-        map.put(true, new ArrayList<>());
-        map.put(false, new ArrayList<>());
-        for (Person person : people) {
-            map.get(person.getAge() >= 18).add(person);
-        }
-        return map;
-    }
-
-    public static Map<Boolean, List<Person>> partitionAdults(List<Person> people) {
         // ToDo
         return null;
     }
@@ -71,6 +45,10 @@ public class Task {
         return null;
     }
 
+    /**
+     * MEDIUM
+     */
+
     public static Map<String,Person> createMapOldJava(List<Person> collection) {
         Map<String,Person> people = new HashMap<>();
         for (Person element : collection) {
@@ -85,6 +63,42 @@ public class Task {
     }
 
 
+    public static Person getOldestPersonOldJava(List<Person> people) {
+        Person oldestPerson = new Person("", 0);
+        for (Person person : people) {
+            if (person.getAge() > oldestPerson.getAge()) {
+                oldestPerson = person;
+            }
+        }
+        return oldestPerson;
+    }
+
+    public static Person getOldestPerson(List<Person> people) {
+        // ToDo
+        return null;
+    }
+
+    /**
+     * HARD
+     */
+
+
+    public static Map<Boolean, List<Person>> partitionAdultsOldJava(List<Person> people) {
+        Map<Boolean, List<Person>> map = new HashMap<>();
+        map.put(true, new ArrayList<>());
+        map.put(false, new ArrayList<>());
+        for (Person person : people) {
+            map.get(person.getAge() >= 18).add(person);
+        }
+        return map;
+    }
+
+    // use partitionBy
+    public static Map<Boolean, List<Person>> partitionAdults(List<Person> people) {
+        // ToDo
+        return null;
+    }
+
     public static List<String> transformListOldJava(List<List<String>> collection) {
         List<String> newCollection = new ArrayList<>();
         for (List<String> subCollection : collection) {
@@ -95,10 +109,9 @@ public class Task {
         return newCollection;
     }
 
+    //use flatMap
     public static List<String> transformList(List<List<String>> collection) {
         // ToDo
         return null;
     }
-
-
 }
