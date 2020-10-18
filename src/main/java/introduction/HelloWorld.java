f00e598c00b16a0d1e6a85176bb4fb0403ef25cc
witkowski.mar@gmail.com <witkowski.mar@gmail.com>
2020-10-18 17:18:07 +0200

    Final code

--- src/main/java/introduction/HelloWorld.java
+++ src/main/java/introduction/HelloWorld.java
@@ -1,8 +1,12 @@
 package introduction;
 
+import org.apache.log4j.Logger;
+
 public class HelloWorld {
+    static Logger log = Logger.getLogger("name");
 
     public static void main(String [ ] args) {
         System.out.println("Hello World");
+        log.info("message");
     }
 }
