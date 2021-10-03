05a07e3cb3a010594f8ae9afe51dfa8b0a0736e7
marcin witkowski <marcin.witkowski@e-qsg.com>
2021-10-03 17:54:27 +0200

    Zmiana komunikatu

--- src/main/java/introduction/HelloWorld.java
+++ src/main/java/introduction/HelloWorld.java
@@ -3,6 +3,6 @@ package introduction;
 public class HelloWorld {
 
     public static void main(String [ ] args) {
-        System.out.println("Hello World");
+        System.out.println("Hello! Witaj!");
     }
 }
