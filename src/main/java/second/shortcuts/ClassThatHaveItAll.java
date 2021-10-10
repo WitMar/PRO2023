0d1a275eb2fae93506612aa584ec009a880a32c8
marcin witkowski <marcin.witkowski@e-qsg.com>
2021-10-10 18:19:39 +0200

    ugly code

--- src/main/java/second/shortcuts/ClassThatHaveItAll.java
+++ src/main/java/second/shortcuts/ClassThatHaveItAll.java
@@ -1,12 +1,4 @@
 package second.shortcuts;
 
-public class ClassThatHaveItAll implements InterfaceOne {
-
-    String name;
-    Integer number;
-
-    @Override
-    public void printMe(String info) {
-
-    }
-}
+public class ClassThatHaveItAll implements InterfaceOne {String name;Integer number;@Override
+    public void printMe(String info) {  } }
