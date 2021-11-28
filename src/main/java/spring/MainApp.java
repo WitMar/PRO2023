a0f927a1cfb0f6554f84400f35d07b4d9874a083
marcin witkowski <marcin.witkowski@e-qsg.com>
2021-11-28 19:04:57 +0100

    code classes

--- src/main/java/spring/MainApp.java
+++ src/main/java/spring/MainApp.java
@@ -2,9 +2,10 @@ package spring;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 import org.springframework.context.ConfigurableApplicationContext;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {HibernateJpaAutoConfiguration.class })
 public class MainApp {
     public static void main(String[] args) {
         SpringApplication bootApp = new SpringApplication(HelloWorldConfig.class);
