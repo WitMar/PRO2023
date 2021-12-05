a7463a95fc3590e6c3966ad14e736fd94b460435
THINK <witkowski.mar@gmail.com>
2021-12-05 20:19:10 +0100

    startClass

--- src/main/java/com/pracownia/spring/SpringApp.java
+++ src/main/java/com/pracownia/spring/SpringApp.java
@@ -3,7 +3,6 @@ package com.pracownia.spring;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
 @EnableJpaRepositories("com.pracownia.spring.repositories")
