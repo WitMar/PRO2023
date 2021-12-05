388b263a3ce50e34f0353aaf5681f33f62f87c09
marcin witkowski <marcin.witkowski@e-qsg.com>
2021-12-05 13:32:40 +0100

    code classes

--- src/main/java/com/pracownia/spring/SpringBootWebApplication.java
+++ src/main/java/com/pracownia/spring/SpringBootWebApplication.java
@@ -1,19 +1,7 @@
 package com.pracownia.spring;
 
-import com.fasterxml.jackson.databind.DeserializationFeature;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
-
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
-import org.springframework.context.annotation.Bean;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
-import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.spi.DocumentationType;
-import springfox.documentation.spring.web.plugins.Docket;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 class SpringBootWebApplication extends SpringBootServletInitializer {
 
