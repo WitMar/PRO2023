0148ab6d7666b13259c7f2c73fefa7fc8e911615
marcin witkowski <marcin.witkowski@e-qsg.com>
2021-12-12 19:51:16 +0100

    code changes fot Tomcat java 1.8 safe

--- src/main/java/com/pracownia/spring/SpringApp.java
+++ src/main/java/com/pracownia/spring/SpringApp.java
@@ -2,12 +2,13 @@ package com.pracownia.spring;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
 @EnableJpaRepositories("com.pracownia.spring.repositories")
-public class SpringApp {
+public class SpringApp extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(SpringApp.class, args);
