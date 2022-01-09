cf038475cf91e1a2774e7e8c34fd14be22a8a555
mawit <marcin.witkowski@rockwool.com>
2022-01-09 20:21:44 +0100

    refactor

--- spring-social/src/main/java/com/example/springsocial/controller/UserController.java
+++ spring-social/src/main/java/com/example/springsocial/controller/UserController.java
@@ -1,15 +1,15 @@
 package com.example.springsocial.controller;
 
-import com.example.springsocial.exception.ResourceNotFoundException;
-import com.example.springsocial.model.User;
-import com.example.springsocial.repository.UserRepository;
-import com.example.springsocial.model.UserPrincipal;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.Authentication;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.example.springsocial.exception.ResourceNotFoundException;
+import com.example.springsocial.model.User;
+import com.example.springsocial.model.UserPrincipal;
+import com.example.springsocial.repository.UserRepository;
 
 @RestController
 public class UserController {
