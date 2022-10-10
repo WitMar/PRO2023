e2358d034684668f3ce44ba2765171e5df49fd35
wojtekgoralewski <wojtek.goralewski1@gmail.com>
2022-10-10 16:47:44 +0200

    pierwszy test zajecia

--- src/main/java/introduction/HelloWorld.java
+++ src/main/java/introduction/HelloWorld.java
@@ -1,8 +1,15 @@
 package introduction;
 
+import org.apache.log4j.Logger;
+
 public class HelloWorld {
 
+    static Logger log = Logger.getLogger("abc");
+
     public static void main(String [ ] args) {
+
+        log.info("wg");
         System.out.println("Hello! Witaj!");
     }
+
 }
