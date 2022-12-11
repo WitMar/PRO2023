de2453080ad15a13a650d291ef26671ed8f19942
marcin witkowski <marcin.witkowski@e-qsg.com>
2022-12-11 19:22:48 +0100

    add swagger

--- src/main/java/com/pracownia/spring/SpringApp.java
+++ src/main/java/com/pracownia/spring/SpringApp.java
@@ -3,14 +3,27 @@ package com.pracownia.spring;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
 @EnableJpaRepositories("com.pracownia.spring.repositories")
+@EnableSwagger2
 public class SpringApp extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(SpringApp.class, args);
+    }
+
+    @Bean
+    public Docket productApi() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select().apis(RequestHandlerSelectors.basePackage("com.pracownia.spring.controllers"))
+                .build();
     }
 
 }
